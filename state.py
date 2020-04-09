39b554a19397cc1ec5b19afc55c6652b6ca8bef4
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-04-09 23:30:49 +0530

    Add win condition

--- state.py
+++ state.py
@@ -6,33 +6,37 @@ class ColumnFullException(Exception):
 class State:
 
     def __init__(self):
-        self.player_turn = True
+        self.human_turn = True
         self.turn_count = 0
         self.board = Board.new()
         self.last_turn = (-1, -1)
 
-    def __str__(self):
-        return '\n'.join([f'Player turn: {self.player_turn}', f'Turn count: {self.turn_count}', self.board.__str__()])
+    def __repr__(self):
+        return 'State(0)'
 
-    def col_check(self, col):
+    def __str__(self):
+        return '\n'.join([f'Player turn: {self.human_turn}', f'Turn count: {self.turn_count}', self.board.__str__()])
+
+    def col_check(self, col, val):
         choice = [row for row in [0, 1, 2, 3] if self.board.check(row, col) == 0]
         if (len(choice) == 0):
             raise ColumnFullException
         else:
-            return choice[0]
+            row = choice[0]
+            self.board.select(row, col, val)
+            self.turn_count += 1 
+            self.last_turn = (row, col)
 
-    def player_choice(self, col):
-        row = self.col_check(col)
-        self.board.select(row, col, 1)
-        self.last_turn = (row, col)
+    def human_choice(self, col):
+        self.col_check(col, 1)
+        self.human_turn = False
 
     def ai_choice(self, col):
-        row = self.col_check(col)
-        self.board.select(row, col, 2)
-        self.last_turn = (row, col)
+        self.col_check(col, 2)
+        self.human_turn = True
 
     def win(self):
-        orientations = [
+        line_offsets = [
             # vertical
             [(0, 0), (1, 0), (2, 0)],
             [(-1, 0), (0, 0), (1, 0)],
@@ -44,4 +48,25 @@ class State:
             [(0, -2), (0, -1), (0, 0)],
         ]
 
-        return any()
+        # helper function to add elements of two tuples
+        def add(tp1, tp2): return (tp1[0] + tp2[0], tp1[1] + tp2[1])
+
+        return any (
+            [
+                # evaluate sum of values calculated from a line
+                sum(values) in [3, 6]
+                for values in [
+                    [
+                        # find position values by adding offset with last turn position
+                        self.board.check(*add(self.last_turn, offset))
+                        # iterate each offset in a single line offset
+                        for offset in line_offset
+                        # only add keep position if it is within board boundaries
+                        if self.board.safe(*add(self.last_turn, offset))
+                    ]
+
+                    # iterate each line offset
+                    for line_offset in line_offsets
+                ]
+            ]
+        )
